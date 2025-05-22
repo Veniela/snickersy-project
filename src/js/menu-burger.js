62c33493d50bcfaf6859324fc1b0dbbf7a69e92f
Evelina Kobyletska <evelina.kobyletska@gmail.com>
2025-05-22 15:39:19 +0200

    add all changes

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -1,16 +1,25 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-open");
-      document.body.classList.toggle("menu-open");
-    }
-  })();
-  
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    menuLinks: document.querySelectorAll(".nav-list-mob a"), // лінки мобільного меню
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  refs.menuLinks.forEach(link => {
+    link.addEventListener("click", closeMenu);
+  });
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+    document.body.classList.toggle("menu-open");
+  }
+
+  function closeMenu() {
+    refs.menu.classList.remove("is-open");
+    document.body.classList.remove("menu-open");
+  }
+})();
